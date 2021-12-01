535f315ccdee0470919ecd8fb7c312eb270912b1
sankalpmukim <sankalpmukim@gmail.com>
2021-12-01 18:43:21 +0530

    theory da + pdfs

--- CyclesheetOne/Q15.java
+++ CyclesheetOne/Q15.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-import java.util.NoSuchElementException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Q15 {
@@ -7,20 +7,28 @@ public class Q15 {
         Scanner sc = new Scanner(System.in);
 
         final int LIMIT = 10;
-        int[] arr = new int[10];
+        int[] iArr = new int[LIMIT];
+        int intPtr = 0, floatPtr = 0;
+        float[] fArr = new float[LIMIT];
 
         int i = 0;
 
         while (i < LIMIT) {
             try {
-                arr[i] = sc.nextInt();
-                i++;
-            } catch (NoSuchElementException e) {
-                e.printStackTrace();
+                iArr[intPtr] = sc.nextInt();
+                intPtr++;
+            } catch (InputMismatchException e) {
+                fArr[floatPtr] = sc.nextFloat();
+                floatPtr++;
             }
+
+            i++;
         }
 
-        System.out.println("Array inputted: " + Arrays.toString(arr));
+        System.out.println("Integer Array inputted: " + Arrays.toString(iArr));
+        System.out.println("Total integer numbers: " + intPtr);
+        System.out.println("Float Array inputted: " + Arrays.toString(fArr));
+        System.out.println("Total float numbers: " + floatPtr);
 
         sc.close();
     }
